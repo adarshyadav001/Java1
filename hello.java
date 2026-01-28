f31dd9abe5e646cc9a6ac19906ee19fc2a8cc6a3
Adarsh Yadav <officialaadi143@gmail.com>
2026-01-28 15:23:06 +0530

    new changes

--- hello.java
+++ hello.java
@@ -1,7 +1,10 @@
-void main(){
-    System.out.println("Hello Buddy");
-}
+
 // Datatypes:char,byte,short,int,boolean,long,float,string
 // Keyboard entry:  With the help of scanner class java input the keyboard entry in easy way . The steps are below
 // import java.util.Scanner
 // create object of scanner class inside main
+// Program structure
+// JVM = Java virtual machine
+// OOP= in java class is a user defined data type and its variable is called object the object is by default referance type
+// The class can hold functions and variables . The functions of the class is called member and variables in the class are called data member 
+// The object of a class can access aby data memeber of class
