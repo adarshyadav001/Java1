46a2b3e058abecb73d479b933982c905a6288d39
Adarsh Yadav <officialaadi143@gmail.com>
2026-01-30 22:29:00 +0530

    added

--- hello.java
+++ hello.java
@@ -8,3 +8,32 @@
 // OOP= in java class is a user defined data type and its variable is called object the object is by default referance type
 // The class can hold functions and variables . The functions of the class is called member and variables in the class are called data member 
 // The object of a class can access aby data memeber of class
+
+
+
+
+/*Static is a keyword that can be prefixed with-
+1. Variables2. Method3. Block
+Static Variables- 
+class s1 = new keyword(s1)
+class s2= new keyword(s2)
+The static variable take a separate memory that will be shared by all the objects of class.
+static variable can be accessed by any member of class
+WAP to find number of objects in a class??
+static member can be accesed by class name but it my also accessed by
+Student s1= new student("Ravi");
+Student s2= new student("Aditya");
+Studnet s3= new studet("Rahul");
+Student s4= new student("Abhi");
+
+When static keyword is prefixed with any method of the class then the method is called static method,it can access only static member of same class directly
+The function can be accessed by class name directly that's why when we execute any java program
+
+
+
+
+
+
+
+ */
+
