9cbd74e5d18b3758f997d1c6d2ad7d3dffdd5487
Adarsh Yadav <officialaadi143@gmail.com>
2026-02-17 09:16:23 +0530

    Added exception handling program

---
+++ Exception handling/Tester.java
@@ -0,0 +1,12 @@
+class Tester {
+    public static void main(String[] args) {
+        int []num = { 2,4,5,6,10};
+        int sum = 0;
+        float count = 5;
+        for(int a:num)
+            sum +=a;
+        float avg =sum/count;
+        System.out.println("Average:" +avg);
+    }
+    
+}
