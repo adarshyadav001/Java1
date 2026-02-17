99e300883b74c5ae41331dc42f27f73f6bc5220b
Adarsh Yadav <officialaadi143@gmail.com>
2026-02-17 09:39:38 +0530

    Refactor Tester class to include user input and exception handling for average calculation

--- Exception handling/Tester.java
+++ Exception handling/Tester.java
@@ -1,12 +1,27 @@
-class Tester {
-    public static void main(String[] args) {
-        int []num = { 2,4,5,6,10};
-        int sum = 0;
-        float count = 5;
-        for(int a:num)
-            sum +=a;
-        float avg =sum/count;
-        System.out.println("Average:" +avg);
+import java.util.Scanner;
+class Tester 
+{
+      public static void main(String[] args) 
+    {
+        try
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter Total number:");
+            int count = scan.nextInt();
+            int []num = { 2,4,5,6,10};
+            int sum = 0;
+            for(int i=0;i<count;i++)
+            {
+              sum = sum + num[i];
+              System.out.println("sum: " +sum);
+            }
+        float avg = sum/count;
+        System.out.println("Average: " +avg);
+
+        }  
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
     
 }
