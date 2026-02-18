422519d1231a2f92d8ce46112ad4eb05c4a25942
Adarsh Yadav <officialaadi143@gmail.com>
2026-02-18 14:10:50 +0530

    Implement Tester1 class for user input and average calculation with exception handling

---
+++ Exception handling/Tester1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Tester1 {
+    public static void main(String[] args) {
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter total number:");
+            int count = scan.nextInt();
+            int[] num = { 2, 4, 5, 6, 10 };
+            int sum = 0;
+            for (int i = 0; i < count; i++) {
+                sum = sum + num[i];
+                System.out.println("sum:" + sum);
+            }
+
+            float avg = sum / count;
+            System.out.println("Average: " + avg);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
