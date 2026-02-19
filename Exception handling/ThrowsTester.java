00d06232723e586245a89f2c712b5eee3581d2a5
Adarsh Yadav <officialaadi143@gmail.com>
2026-02-19 10:08:54 +0530

    Refactor ATM class for improved exception handling in deposit and withdraw methods; enhance formatting in ThrowsTester

--- Exception handling/ThrowsTester.java
+++ Exception handling/ThrowsTester.java
@@ -1,36 +1,46 @@
-class ATM{
+class ATM {
     private int balance = 5000;
+
     public int getBalance() {
         return balance;
     }
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
-    public void deposit(int amt) throws Exception{
-        System.out.println("Deposit: " +amt);
-        throw new Exception("-ve amount is incvalid to deposit");
-        balance += amt;
 
+    public void deposit(int amt) throws Exception {
+        if (amt < 0) {
+            throw new Exception("Negative amount is invalid to deposit");
+        }
+        balance += amt;
+        System.out.println("Deposit: " + amt);
     }
-    public void withdraw(int amt){
-        if(amt>balance)
-            System.out.println("Insufficient balance");
-        else
-            System.out.println("Withdraw: " +amt);
+
+    public void withdraw(int amt) throws Exception {
+        if (amt < 0) {
+            throw new Exception("Negative amount is invalid to withdraw");
+        }
+        if (amt > balance) {
+            throw new Exception("Insufficient balance");
+        }
         balance -= amt;
+        System.out.println("Withdraw: " + amt);
     }
 }
+
 public class ThrowsTester {
     public static void main(String[] args) {
         try {
             ATM a1 = new ATM();
-            System.out.println("Balance: " +a1.getBalance());
+            System.out.println("Balance: " + a1.getBalance());
+
             a1.deposit(2000);
-            System.out.println("Balance: " +a1.getBalance());
+            System.out.println("Balance: " + a1.getBalance());
+
             a1.withdraw(2500);
-             System.out.println("Balance: " +a1.getBalance());
-            
+            System.out.println("Balance: " + a1.getBalance());
+
         } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        } finally {
+            System.out.println("Finally reached the end");
         }
     }
 }
