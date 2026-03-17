500a550db5b3dd7dac12fe4f6af8b0d1a885a4a5
Adarsh Yadav <officialaadi143@gmail.com>
2026-03-17 15:04:19 +0530

    Refactor Testerr class to improve string formatting and add missing import

--- Testerr.java
+++ Testerr.java
@@ -1,8 +1,10 @@
- class Testerr {
+import java.util.function.*;
+
+class Testerr {
     public String hello(int a) {
-        return a + "";
+        return "Value=" +a;
     }
-    
+    Function <Integar,String> hello2 = (i)-> "Value"+i;
     public static void main(String[] args) {
         Testerr t = new Testerr();
         System.out.println(t.hello(25));
