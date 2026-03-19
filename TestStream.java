7e490114bdff02876c5d85b7fae14a0cfad8e4ed
Adarsh Yadav <officialaadi143@gmail.com>
2026-03-19 16:00:01 +0530

    Add StreamOp and TestStream classes for stream operations and testing

---
+++ TestStream.java
@@ -0,0 +1,14 @@
+import java.util.stream.Stream;
+void main(){
+    int []arr = {1,4,3,2,6,7,8,10,2};
+    Stream<Integar> st1 = Arryas.stream(arr).boxed();
+
+    int []arr2 = {1, 4, 3, 2, 6, 7, 8, 10, 2};
+    Stream<Integar> st2 = Arryas.stream(arr2);
+
+    st1.forEach(s->System.out.println(s+","));
+
+    Steam<String> st3 = Stream.of("Raman",)
+
+
+}
