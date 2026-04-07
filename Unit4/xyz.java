5b3ea14f23790e52881a9c0223e1c6750c8ed59c
Adarsh Yadav <officialaadi143@gmail.com>
2026-04-07 14:56:33 +0530

    Add SetTester class with HashSet operations and examples

--- Unit4/xyz.java
+++ Unit4/xyz.java
@@ -1,3 +1,13 @@
-public class xyz {
-    
+import java.util.*;
+void main(){
+    Set<Integer> list = new HashSet<>();
+    list.add(1);
+    list.add(2);
+    list.add(null);
+    list.add(-5);
+    list.add(0);
+
+    System.out.println(list);
+    list.addAll(Arrays.asList(1, 2, 3, 4, 5));
+    System.out.println(list);
 }
